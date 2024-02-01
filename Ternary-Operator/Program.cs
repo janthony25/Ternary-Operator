c6054b77610c0461fcde9c395e95bae37ae644cf
janthony25 <junanthony.business@gmail.com>
2024-02-01 22:59:24 +1300

    Add project files.

---
+++ Ternary-Operator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TernaryOperator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // (condition) ? x : y
+
+            double temperature = 20;
+
+          string message =  (temperature >= 15) ? "It is hot outside!" : "It is cold outside!";
+
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
